adb28dd5ef3d65020f3fe83fc87d8f86e8fbccac
malbogle <josephjasson22@gmail.com>
2021-09-09 08:05:49 +0200

    Added health ticking functionality

---
+++ Level1.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var player1: = $Player1
+onready var player2: = $Player2
+onready var timer: = $HealthTickTimer
+
+func _on_HealthTickTimer_timeout() -> void:
+	player1.health.set_current(player1.health.current-10)
+	player2.health.set_current(player2.health.current-10)
+	
+
+#Implement rest of death logic here
+func _on_Player1_player_death() -> void:
+	timer.stop()
+
+#Implement rest of death logic here
+func _on_Player2_player_death() -> void:
+	timer.stop()
--- Player.gd
+++ Player.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 class_name Player 
 
-var lives: = 3
 var velocity: = Vector2(0,0)
 var speed: = 500
 const JUMPFORCE: = -1100
