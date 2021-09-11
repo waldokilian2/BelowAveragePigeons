5e66b6342aae458452f66c017ed069f3c29aecf6
malbogle <josephjasson22@gmail.com>
2021-09-11 11:24:49 +0200

    idk whats changed

--- Level1.gd
+++ Level1.gd
@@ -5,8 +5,8 @@ onready var player2: = $Player2
 onready var timer: = $HealthTickTimer
 
 func _on_HealthTickTimer_timeout() -> void:
-	player1.health.set_current(player1.health.current-10)
-	player2.health.set_current(player2.health.current-10)
+	player1.health.set_current(player1.health.current-1)
+	player2.health.set_current(player2.health.current-1)
 	
 #Implement rest of death logic here
 func _on_Player1_player_death() -> void:
