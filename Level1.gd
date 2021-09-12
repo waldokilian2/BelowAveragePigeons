06a03dabb2c1d7785eb52fec36b6525b9390648a
Thabiso Makolana <thabisomak@bbd.co.za>
2021-09-12 10:49:36 +0200

    added instances of food objects, randomised position for berry instance

--- Level1.gd
+++ Level1.gd
@@ -4,6 +4,18 @@ onready var player1: = $Player1
 onready var player2: = $Player2
 onready var timer: = $HealthTickTimer
 
+func _ready():
+	var berry = preload("res://Berry.tscn").instance()
+	var carrot = preload("res://Carrot.tscn").instance()
+	var clover = preload("res://Clover.tscn").instance()
+	var pom = preload("res://Pom.tscn").instance()
+	
+	var random_x = rand_range(0, 1920)
+	var random_y = rand_range(0, 1080)
+	berry.global_position = Vector2(random_x, random_y)
+	
+	add_child(berry)
+
 func _on_HealthTickTimer_timeout() -> void:
 	player1.health.set_current(player1.health.current-1)
 	player2.health.set_current(player2.health.current-1)
